ceb5d9d032f82aa61f5303245fc57bef04674f6a
Kevin <dungle-scrubs@users.noreply.github.com>
2026-02-13 12:48:49 +0700

    chore: gitignore .astro build cache

--- apps/docs/.astro/content-assets.mjs
+++
@@ -1,4 +0,0 @@
-
-import __ASTRO_IMAGE_IMPORT_25JXob from "../../assets/logo.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fdocs%2Findex.mdx";
-export default new Map([["../../assets/logo.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fdocs%2Findex.mdx", __ASTRO_IMAGE_IMPORT_25JXob]]);
-		
